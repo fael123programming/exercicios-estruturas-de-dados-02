df90359611c0ef0f5ff5f93f3cb7a706c718e628
Fael123Programming <rafaelfonseca1020@gmail.com>
2021-11-02 21:21:36 -0300

    New Files

---
+++ src/threads/ex1/MathSub.java
@@ -0,0 +1,16 @@
+package threads.ex1;
+
+import java.util.Scanner;
+
+public class MathSub implements Runnable {
+    @Override
+    public void run(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Number 1: ");
+        double n1 = input.nextDouble();
+        System.out.print("Number 2: ");
+        double n2 = input.nextDouble();
+        System.out.println("-------------------------------");
+        System.out.println("Subtraction is " + (n1 - n2));
+    }
+}
---
+++ src/threads/ex1/TestThreads.java
@@ -0,0 +1,14 @@
+package threads.ex1;
+
+public class TestThreads {
+    public static void main(String[] args) {
+        op("sub");
+    }
+
+    private static void op(String type) {
+        switch(type.toUpperCase()) {
+            case "SUM" -> new Thread(new MathSum()).start();
+            case "SUB" -> new Thread(new MathSub()).start();
+        }
+    }
+}
