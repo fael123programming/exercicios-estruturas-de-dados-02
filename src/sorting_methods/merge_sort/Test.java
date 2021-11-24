fe566c4d5107bfd0802f9f1a05ff7205bcd45608
Fael123Programming <rafaelfonseca1020@gmail.com>
2021-11-24 15:31:07 -0300

    New Files

--- src/sorting_methods/merge_sort/Test.java
+++ src/sorting_methods/merge_sort/Test.java
@@ -1,9 +1,13 @@
 package sorting_methods.merge_sort;
 
 import time_measurement.Time;
+import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
+        int[] n1 = {11, 1, 10, 2, 9, 3, 8, 4, 2, 11};
+        MergeSort.sort(n1);
+        System.out.println(Arrays.toString(n1));
         final int SIZE = 173539323;
         int[] numbers = new int[SIZE];
         for (int i = 0; i < SIZE; i++) {
