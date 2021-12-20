449828d2f503867dfb091c2f67735c8f2faf3ec9
Fael123Programming <rafaelfonseca1020@gmail.com>
2021-12-20 09:10:52 -0300

    Final LSD implementation

--- src/sorting_methods/key_based_sorts/radix_sort/RadixSort.java
+++ src/sorting_methods/key_based_sorts/radix_sort/RadixSort.java
@@ -9,8 +9,9 @@ package sorting_methods.key_based_sorts.radix_sort;
  * There are two versions of radix sort: MSD (Most Significant Digit) based and LSD (Least Significant Digit) based;
  * MSD: evaluates the keys as with strings of characters (lexicographically);
  * LSD: evaluates the keys as with integer numbers (from right places to left ones).
- * Time cost:
- * LSD: O(n*k) where n is the size of the input and k is the medium size of the keys;
+ * Time complexity: O(d * (n + k)) where n is the size of the input, k is the medium size of the keys and d is the quantity
+ * of digits of the maximum value (in module) in the input;
+ * Space complexity: O(n + k) where n is the size of the input and k is the medium size of the keys.
  */
 
 public class RadixSort {
@@ -19,27 +20,27 @@ public class RadixSort {
     }
 
     public static void LSDSort(int[] numbers) {
-//      Let 'max' be the maximum key found in 'numbers', then counting sort will be
+//      Let 'max' be the maximum key found in 'numbers' (not considering sign), then counting sort will be
 //      executed the quantity of digits 'max' has.
-//      First, get the maximum number in numbers and check whether there is at least one negative number in 'numbers'.
-        int max = max(numbers);
-        int executions = digits(max); //Counting sorts to do.
-        int currentPos = 0; //Current digit being sorted and considered.
+//      First, get the quantity of digits of the maximum number (in module) in 'numbers' and check whether
+//      there is at least one negative number there.
+        int executions = digits(maxAbsolute(numbers)); //Counting sorts to do.
+        int currentPos = 0; //Current positional digit being sorted and considered.
         int[] count; //Array to contain the frequencies of the digits of the keys.
-        int[] sorted = new int[numbers.length];
-        int mostNegative = 0;
+        final int[] sorted = new int[numbers.length];
+        int mostNegative; //To contain, if existent, the less negative digit.
+        final int alphabetSize = 10; //From 0 to 9 we have 10 symbols.
         while (executions > 0) {
-            if (executions == 1) {
-//             If we get here, it means that we are analyzing the MSD of each key.
-//             This is the only digit that can have the "-" sign.
-                for (int i : numbers)
-                    if (digitAt(currentPos, i) < mostNegative)
-                        mostNegative = digitAt(currentPos, i);
-                mostNegative *= -1;
-                count = new int[10 + mostNegative];
-            } else count = new int[10];
-            for (int i : numbers) //Computing the frequencies.
-                count[digitAt(currentPos, i) + mostNegative]++;
+            mostNegative = 0;
+            for (int i : numbers) //Getting the less negative digit of a key, if existent.
+                if (digitAt(currentPos, i) < mostNegative)
+                    mostNegative = digitAt(currentPos, i);
+            mostNegative *= -1; //Putting it as its absolute value.
+            count = new int[alphabetSize + mostNegative];
+//          We have to build an array of size "alphabetSize + mostNegative" because of our normalization
+//          when computing each frequency inside count.
+            for (int i : numbers) //Computing the frequencies and normalizing.
+                count[digitAt(currentPos, i) + mostNegative]++; //If there is no negative number, this sum will be 0.
             for (int i = 0; i < count.length - 1; i++) //Updating our array of frequencies.
                 count[i + 1] += count[i];
             for (int i = numbers.length - 1; i > -1; i--)
@@ -48,7 +49,6 @@ public class RadixSort {
             executions--;
             currentPos++;
         }
-        System.arraycopy(sorted, 0, numbers, 0, numbers.length);
     }
 
     /**
@@ -56,14 +56,18 @@ public class RadixSort {
      * position in an integer number.
      *
      * @param position the position of the desired digit in number
-     * @param number   the integer number to get the digit
+     * @param number the integer number to get the digit
      * @return the digit at position "position" in number if existent. If position is < 0, then -1 is returned.
      * If greater than the quantity of digits number has, then 0 is returned.
      */
     private static int digitAt(int position, int number) {
         if (position < 0)
             return -1;
-        return number % (int) Math.pow(10, position + 1) / (int) Math.pow(10, position);
+        int digit = number % (int) Math.pow(10, position + 1) / (int) Math.pow(10, position);
+        if (position == digits(number) - 1) //The M.S.D of the number.
+            return digit; //As it is with sign or not.
+        else
+            return digit < 0 ? digit * -1 : digit; //Only the M.S.D of a number can carry sign.
     }
 
     /**
@@ -78,21 +82,23 @@ public class RadixSort {
             number *= -1;
         int digits = 0;
         for (; (number /= 10) > 0; digits++);
+//      As number / 10 is an integer division it will reach zero in some time.
         return ++digits;
     }
 
     /**
-     * Returns the greatest value found in an array of integers.
+     * Returns the greatest value (in module) found in an array of integers.
      * @param numbers the array where to search
-     * @return the greatest value found in the array numbers
+     * @return the greatest value (in module) found in 'numbers'
      */
-    private static int max (int[] numbers) {
-        int max = 0;
+    private static int maxAbsolute(int[] numbers) {
+        int max = 0, aux;
         for (int i = 0; i < numbers.length; i++) {
+            aux = numbers[i] < 0 ? numbers[i] * -1 : numbers[i]; //Absolute value!!!
             if (i == 0)
-                max = numbers[0];
-            else if (numbers[i] > max)
-                max = numbers[i];
+                max = aux;
+            else if (aux > max)
+                max = aux;
         }
         return max;
     }
--- src/sorting_methods/key_based_sorts/radix_sort/Test.java
+++ src/sorting_methods/key_based_sorts/radix_sort/Test.java
@@ -4,13 +4,9 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        int[] numbers = {220, -740, 800, 120, 150, -200, 900};
+        int[] numbers = {-67, 345, 90, 0, -100, -678, -5, -3, -8976, -10, -352354341, -100, -450};
         System.out.println(Arrays.toString(numbers));
         RadixSort.LSDSort(numbers);
         System.out.println(Arrays.toString(numbers));
-//        System.out.println(RadixSort.max(numbers));
-//        System.out.println(RadixSort.digits(RadixSort.max(numbers)));
-//        for (int i = 0; i < RadixSort.digits(RadixSort.max(numbers)); i++)
-//            System.out.println(RadixSort.digitAt(i, RadixSort.max(numbers)));
     }
 }
