b3bf3a914fc91904c15ee16e59b4cc4f0c8ebd8b
Fael123Programming <rafaelfonseca1020@gmail.com>
2021-12-20 09:18:30 -0300

    Radix Sort and Counting Sort

--- src/sorting_methods/key_based_sorts/counting_sort/Test.java
+++ src/sorting_methods/key_based_sorts/counting_sort/Test.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        int [] numbers = new int[100];
-        for (int i = 1; i < 101; i++)
+        int[] numbers = new int[100];
+        for (int i = 1; i < 101; i++) //1 through 100.
             numbers[i - 1] = 50 - i;
         System.out.println(Arrays.toString(numbers));
         CountingSort.sort(numbers);
